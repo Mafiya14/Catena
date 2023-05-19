07ac842a5733be838addbfa6944313351bf2062a
Denis Litvinets <denis.litvinets@gmail.com>
2023-05-19 17:39:02 +0300

    add some scripts and water drop object

---
+++ Assets/Scripts/Character/CharacterBase.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterBase : MonoBehaviour
+{
+    [SerializeField] protected float _moveSpeed;
+}
