4c3587f66e8c8355f1b494df2acb6e8deb9a4539
Denis Litvinets <denis.litvinets@gmail.com>
2023-05-21 18:26:58 +0300

    add damper move animation

--- Assets/Scripts/Level/Sections/PipeWithDamper.cs
+++ Assets/Scripts/Level/Sections/PipeWithDamper.cs
@@ -1,8 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class PipeWithDamper : PipeSection
 {
-    
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _raisedPosition;
+    [SerializeField] private Transform _damper;
+
+    private float _loweredPosition;
+    private bool _damperIsRaised;
+    private bool _isMoving;
+
+    private void Start()
+    {
+        _loweredPosition = _damper.localPosition.y;
+    }
+
+    private void OnMouseUp()
+    {
+        if (_isMoving)
+            return;
+
+        if (_damperIsRaised)
+        {
+            Move(_loweredPosition);
+            _damperIsRaised = false;
+        }
+        else
+        {
+            Move(_raisedPosition);
+            _damperIsRaised = true;
+        }
+    }
+
+    private async void Move(float end)
+    {
+        Vector3 targetPosition = new(_damper.localPosition.x, end, _damper.localPosition.z);
+        _isMoving = true;
+
+        while (_damper.localPosition != targetPosition)
+        {
+            _damper.localPosition = Vector3.MoveTowards(_damper.localPosition, targetPosition, _moveSpeed * Time.deltaTime);
+
+            await Task.Yield();
+        }
+
+        _isMoving = false;
+    }
 }
