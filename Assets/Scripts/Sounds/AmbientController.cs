2f1c1c0512204de40f47df64c65dc3a8639102be
Denis Litvinets <denis.litvinets@gmail.com>
2023-05-23 18:41:39 +0300

    add sounds and ambient controller

---
+++ Assets/Scripts/Sounds/AmbientController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientController : MonoBehaviour
+{
+    public static AmbientController Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
+}
